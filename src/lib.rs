03af4a6fbd9426612122357a5c7044785fe3c641
VictorPS <victorpsilvano@gmail.com>
2026-03-14 21:40:36 +0000

    Implement the search function, which also makes the test pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  unimplemented!();
+  let mut result: Vec<&'a str> = Vec::new();
+  for line in contents.lines() {
+    if line.contains(query) {
+      result.push(line);
+    }
+  }
+  result
 }
 
 #[cfg(test)]
